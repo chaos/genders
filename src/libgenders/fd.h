74d1be8d8abe7ace96509e9192e4a9b06f4e405f
Albert Chu <chu11@llnl.gov>
2003-12-30 21:47:13 +0000

    updated copyright globally

--- src/libgenders/fd.h
+++ src/libgenders/fd.h
@@ -1,27 +1,27 @@
 /*****************************************************************************
- *  $Id: fd.h,v 1.1 2003-11-03 17:37:40 achu Exp $
+ *  $Id: fd.h,v 1.2 2003-12-30 21:47:13 achu Exp $
  *****************************************************************************
- *  This file is part of the Munge Uid 'N' Gid Emporium (MUNGE).
- *  For details, see <http://www.llnl.gov/linux/munge/>.
- *  UCRL-CODE-2003-???.
- *
- *  Copyright (C) 2001-2003 The Regents of the University of California.
+ *  Copyright (C) 2001-2002 The Regents of the University of California.
  *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
  *  Written by Chris Dunlap <cdunlap@llnl.gov>.
- *
- *  This is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- *  for more details.
- *
- *  You should have received a copy of the GNU General Public License;
- *  if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- *  Suite 330, Boston, MA  02111-1307  USA.
+ *  UCRL-CODE-2002-009.
+ *  
+ *  This file is part of ConMan, a remote console management program.
+ *  For details, see <http://www.llnl.gov/linux/conman/>.
+ *  
+ *  ConMan is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *  
+ *  ConMan is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *  
+ *  You should have received a copy of the GNU General Public License along
+ *  with ConMan; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  *****************************************************************************/
 
 
--- src/libgenders/genders.c
+++ src/libgenders/genders.c
@@ -1,7 +1,28 @@
-/*
- * $Id: genders.c,v 1.71 2003-11-07 16:38:10 achu Exp $
- * $Source: /g/g0/achu/temp/genders-cvsbackup-full/genders/src/libgenders/genders.c,v $
- */
+/*****************************************************************************\
+ *  $Id: genders.c,v 1.72 2003-12-30 21:47:13 achu Exp $
+ *****************************************************************************
+ *  Copyright (C) 2001-2003 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Jim Garlick <garlick@llnl.gov> and Albert Chu <chu11@llnl.gov>.
+ *  UCRL-CODE-2003-004.
+ *
+ *  This file is part of Genders, a cluster configuration database.
+ *  For details, see <http://www.llnl.gov/linux/genders/>.
+ *
+ *  Genders is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  Genders is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Genders; if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+\*****************************************************************************/
 
 #include <ctype.h>
 #include <fcntl.h>
