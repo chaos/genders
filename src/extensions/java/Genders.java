805edd6e31e478a60ae8caa595005813e356d239
Albert Chu <chu11@llnl.gov>
2012-08-23 17:47:03 +0000

    save

---
+++ src/extensions/java/Genders.java
@@ -0,0 +1,28 @@
+class Genders
+{
+    private long gh = 0;
+    public Genders()
+    {
+	gh = 1;
+    }
+    public int getnumnodes()
+    {
+	return 5;
+    }
+    /*
+    public int getnumattrs()
+    {
+    }
+    public int getmaxattrs()
+    {
+    }
+    public long ghaddr()
+    {
+	return gh;
+    }
+    static
+    {
+	System.loadLibrary("Genders");
+    }
+    */
+}
---
+++ src/extensions/java/Maintest.java
@@ -0,0 +1,9 @@
+class Maintest
+{
+    public static void main(String[] args)
+    {
+	Genders gh = new Genders();
+	System.out.println("Hello World");
+	System.out.println(gh.getnumnodes());
+    }
+}
