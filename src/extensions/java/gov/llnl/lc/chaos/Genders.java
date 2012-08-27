5dce63e7bc72570b5aadd6a3072f7ae2b9651075
Albert Chu <chu11@llnl.gov>
2012-08-27 17:05:35 +0000

    begin java docs

--- src/extensions/java/gov/llnl/lc/chaos/Genders.java
+++ src/extensions/java/gov/llnl/lc/chaos/Genders.java
@@ -1,25 +1,57 @@
 package gov.llnl.lc.chaos;
 
+/**
+ * Class Name: Genders
+ *
+ * Java JNI extensions wrapper around libgenders C library.  Most
+ * library functions behave similarly to C api functions with minor
+ * exceptions documented below.
+ */
 public class Genders
 {
     private long gh_addr = 0;
 
     private native int genders_constructor(String filename);
 
+    /**
+     * Creates a Genders object, loading the default genders database
+     */
     public Genders()
     {
 	genders_constructor(null);
     }
 
+    /**
+     * Creates a Genders object, loading the specified genders database
+     */
     public Genders(String filename)
     {
 	genders_constructor(filename);
     }
 
+    /**
+     * Returns number of nodes parsed in the genders database
+     *
+     * @return number of nodes on success
+     * @throws GendersException on error
+     */
     public native int getnumnodes() throws GendersException;
 
+    /**
+     * Returns number of attributes parsed in the genders database
+     *
+     * @return number of attributes on success
+     * @throws GendersException on error
+     */
     public native int getnumattrs() throws GendersException;
 
+    /**
+     * Returns maximum number of attributes of any one node parsed in
+     * the genders database
+     *
+     * @return maximum number of attributes of any one node on success
+     * @throws GendersException on error
+     */
     public native int getmaxattrs() throws GendersException;
 
     public native String getnodename() throws GendersException;
