95377703479c9abeeda7efea0a715910b0f4b069
Albert Chu <chu11@llnl.gov>
2015-06-24 14:22:10 -0700

    Add missing copyright headers to java files

--- src/extensions/java/gov/llnl/lc/chaos/GendersExceptionParse.java
+++ src/extensions/java/gov/llnl/lc/chaos/GendersExceptionParse.java
@@ -1,3 +1,26 @@
+/*****************************************************************************\
+ *  Copyright (C) 2007-2011 Lawrence Livermore National Security, LLC.
+ *  Copyright (C) 2001-2007 The Regents of the University of California.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Jim Garlick <garlick@llnl.gov> and Albert Chu <chu11@llnl.gov>.
+ *  UCRL-CODE-2003-004.
+ *
+ *  This file is part of Genders, a cluster configuration database.
+ *  For details, see <http://www.llnl.gov/linux/genders/>.
+ *
+ *  Genders is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  Genders is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with Genders.  If not, see <http://www.gnu.org/licenses/>.
+\*****************************************************************************/
 package gov.llnl.lc.chaos;
 
 /**
